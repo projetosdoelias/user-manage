d45e6c2f043841b548102d49225f17679f292d0b
Elias Lourenço <elias.oliveira@novaescola.org.br>
2024-04-09 23:56:07 -0300

    adding mongoose dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URL_CONNECTION || 'mongodb://127.0.0.1:27017/user-manage'),
+    UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule { }
+
